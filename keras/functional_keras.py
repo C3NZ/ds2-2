0200c86d53fe99723b6e9cfb66dfb9a2550cee27
C3NZ <cenz@cenz.io>
2019-06-05 15:10:53 -0700

    [feat] rewrite the functional keras demo'

--- keras/functional_keras.py
+++ keras/functional_keras.py
@@ -7,19 +7,34 @@ from sklearn.preprocessing import LabelEncoder
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
-y_train_one_hot = np_utils.to_categorical(y_train)
-y_test_one_hot = np_utils.to_categorical(y_test)
-
 # print(y_one_hot)
 
-inp = Input(shape=(4,))
-x = Dense(16, activation="sigmoid")(inp)
-out = Dense(3, activation="softmax")(x)
-model = Model(inputs=inp, outputs=out)
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-model.fit(X_train, y_train_one_hot, epochs=100, batch_size=1, verbose=0)
-loss, accuracy = model.evaluate(X_test, y_test_one_hot, verbose=0)
-print("Accuracy = {:.2f}".format(accuracy))
+
+def create_model() -> Model:
+    """
+        Create the model.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0
+    )
+
+    y_train_one_hot = np_utils.to_categorical(y_train)
+    y_test_one_hot = np_utils.to_categorical(y_test)
+
+    inp = Input(shape=(4,))
+    x = Dense(16, activation="sigmoid")(inp)
+    out = Dense(3, activation="softmax")(x)
+    model = Model(inputs=inp, outputs=out)
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+    model.fit(X_train, y_train_one_hot, epochs=100, batch_size=1, verbose=0)
+    loss, accuracy = model.evaluate(X_test, y_test_one_hot, verbose=0)
+    print("Accuracy = {:.2f}".format(accuracy))
+    return model
+
+
+def test_model(model: Model) -> None:
+    """
+        Test the model.
+    """
